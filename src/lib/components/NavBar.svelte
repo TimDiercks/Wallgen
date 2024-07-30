603959eeb090b2b9f19cafe603d3f155bf060567
Tim Diercks <89611552+TimDiercks@users.noreply.github.com>
2024-07-30 23:20:50 +0200

    Add pipeline to lint changed files (#9)
    * Add pipeline to lint changed files
    
    * Fix naming
    
    * Add pre-commit hash and fix errors

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,68 +1,61 @@
 <script>
-    import logo from '$lib/assets/logo.png'
+	import logo from '$lib/assets/logo.png'
 </script>
 
 <nav class="outer">
-    <div class="inner">
-        <div class="logo-container">
-            <img src={logo} alt="Wallgen logo"/>
-            <h1>Wallgen</h1>
-        </div>
-
-    </div>
+	<div class="inner">
+		<div class="logo-container">
+			<img src={logo} alt="Wallgen logo" />
+			<h1>Wallgen</h1>
+		</div>
+	</div>
 </nav>
 
 <style>
+	.outer {
+		position: sticky;
+		background-color: #0a1a3b;
+		top: 0;
+		height: 6vh;
+		padding: 1.5rem;
+		margin-bottom: 2rem;
+		contain: content;
+		box-shadow: white 0px 4px 2px;
+	}
 
-    .outer {
-        position: sticky;
-        background-color: #0a1a3b;
-        top: 0;
-        height: 6vh;
-        padding: 1.5rem;
-        margin-bottom: 2rem;
-        contain: content;
-        box-shadow: white 0px 4px 2px;
-    }
+	.logo-container {
+		display: flex;
+		align-items: center;
+		gap: 20px;
+		font-size: 1.5em;
+	}
 
+	.inner {
+		display: flex;
+		margin: 0 10%;
+		height: 100%;
+	}
 
-    .logo-container {
-        display: flex;
-        align-items: center;
-        gap: 20px;
-        font-size: 1.5em;
-    }
+	.logo-container > img {
+		height: 100%;
+		object-fit: contain;
+	}
 
-    .inner {
-        display: flex;
-        margin: 0 10%;
-        height: 100%;
-    }
+	@media only screen and (max-width: 768px) {
+		.logo-container {
+			flex-direction: column;
+			gap: 5px;
+			width: 50px;
+			font-size: 1em;
+		}
 
-    .logo-container > img {
-        height: 100%; 
-        object-fit: contain;
-    }
+		.logo-container > img {
+			width: 100%;
+			object-fit: contain;
+		}
 
-
-    @media only screen and (max-width: 768px) {
-
-        .logo-container {
-            flex-direction: column;
-            gap: 5px;
-            width: 50px;
-            font-size: 1em;
-        }
-
-        
-        .logo-container > img {
-            width: 100%; 
-            object-fit: contain;
-        }
-
-
-        .inner {
-            margin: 0 20px;
-        }
-    }
+		.inner {
+			margin: 0 20px;
+		}
+	}
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,42 +1,43 @@
 <script>
-    import NavBar from "$lib/components/NavBar.svelte";
+	import NavBar from '$lib/components/NavBar.svelte'
 </script>
 
 <NavBar />
 
 <div class="content">
-    <slot />
+	<slot />
 </div>
 
 <style>
-   @font-face {
-        font-family: 'Source Code Pro';
-        font-style: normal;
-        font-weight: 400;
-        src: url(https://fonts.gstatic.com/s/sourcecodepro/v23/HI_diYsKILxRpg3hIP6sJ7fM7PqPMcMnZFqUwX28DMyQtMlrTA.woff2) format('woff2');
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-    } 
-    
-    :global(*) {
-        padding: 0;
-        margin: 0;
-    }
+	@font-face {
+		font-family: 'Source Code Pro';
+		font-style: normal;
+		font-weight: 400;
+		src: url(https://fonts.gstatic.com/s/sourcecodepro/v23/HI_diYsKILxRpg3hIP6sJ7fM7PqPMcMnZFqUwX28DMyQtMlrTA.woff2)
+			format('woff2');
+		unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304,
+			U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215,
+			U+FEFF, U+FFFD;
+	}
 
-    :global(body) {
-        background-color: #0a1a3b;
-        color: #d9d9d9;
-        font-family: 'Source Code Pro';
-    }
+	:global(*) {
+		padding: 0;
+		margin: 0;
+	}
 
-    :global(.content) {
-        padding: 0 2rem;
-    }
+	:global(body) {
+		background-color: #0a1a3b;
+		color: #d9d9d9;
+		font-family: 'Source Code Pro';
+	}
 
+	:global(.content) {
+		padding: 0 2rem;
+	}
 
-    @media only screen and (max-width: 768px) {
-        :global(.content) {
-            padding: 0 1rem;
-        }
-    }
-    
+	@media only screen and (max-width: 768px) {
+		:global(.content) {
+			padding: 0 1rem;
+		}
+	}
 </style>
