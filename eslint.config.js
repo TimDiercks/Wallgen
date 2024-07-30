87b549fc6dae22726554f51f197ef99f2892fbb1
Tim Diercks <tim.r.diercks@gmail.com>
2024-07-30 23:19:00 +0200

    Add pre-commit hash and fix errors

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import js from '@eslint/js'
 import ts from 'typescript-eslint'
-import svelte, { rules } from 'eslint-plugin-svelte'
+import svelte from 'eslint-plugin-svelte'
 import prettier from 'eslint-config-prettier'
 import globals from 'globals'
 
